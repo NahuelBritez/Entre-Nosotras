691f2445c1af28554a019d16c0c4578796214310
Nahuel Britez <94577549+NahuelBritez@users.noreply.github.com>
2022-12-15 19:43:37 -0300

    Correciones en los estilos y urls  (#2)
    * Cache añadido a gitignore
    
    * Arreglos en el index
    
    * Sección de noticias agregada a las url y navbar
    
    * Errores corregidos en los templates de registro, inicio y noticias
    
    * Correcciones de nombres en los html
    
    * Definición de las url  de registro e ingreso

--- sistema/views.py
+++ sistema/views.py
@@ -5,3 +5,12 @@ from django.views.generic import ListView
 class NoticiaListview(ListView):
     model = Noticia
     template_name = 'index.html'
+
+def noticias(request):
+    return render(request,'noticias.html')
+
+def registro(request):
+    return render(request,'registrarse.html')
+
+def ingresar(request):
+    return render(request,'ingresar.html')
